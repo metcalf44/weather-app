de16249a2384c4d917c069640197f535139cea00
Ernie <emetcalf44@gmail.com>
2021-06-11 15:03:44 +0100

    adding forecast summary

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
 import LocationDetails from "./LocationDetails";
+import ForecastSummary from "./ForecastSummary";
 import "../styles/App.css";
 
-const App = ({ location }) => {
+const App = ({ location, forecast }) => {
   const {city, country} = location; 
+  const {date, maxTemp, description} = forecast
   return (
     <div className="Weather-app">
       <LocationDetails 
       city={city} 
-      country={country} />
+      country={country} />,
+    <ForecastSummary 
+      date={date}
+      maxTemp={maxTemp}
+      description={description}
+    />
     </div>
   );
 }
+
 
 App.propTypes = {
   location: PropTypes.shape({
     city: PropTypes.string,
     country: PropTypes.string,
   }).isRequired,
+  ForecastSummary: PropTypes.shape({
+    date: PropTypes.string,
+    maxTemp: PropTypes.string,
+    description: PropTypes.string
+  })
 };
 
 export default App;
---
+++ src/components/ForecastSummary.js
@@ -0,0 +1,43 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ForecastSummary = (props) => {
+    const ForecastSummary = (props) => {
+        const {
+            date,
+            description,
+            icon,
+            temperature
+        } = props;
+    }
+    return (
+        <div class="forecastSummary">
+            <div className="forecast-summary__date">
+                {date}
+            </div>
+            <div className="forecast-summary__icon" data-testid="forecast-icon">
+                {icon}
+            </div>
+            <div className="forecast-summary__temperature">
+                {temperature.max}
+                &deg;C
+            </div>
+            <div className="forecast-summary__description">
+                {description}
+            </div>
+        </div>
+    )
+}
+
+
+export default ForecastSummary;
+
+ForecastSummary.propTypes = {
+    date: PropTypes.number.isRequired,
+    description: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+    temperature: PropTypes.shape({
+        min: PropTypes.number,
+        max: PropTypes.number,
+    }).isRequired,
+}
